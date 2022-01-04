bd5dc6bd11b9d45aceb0758293119db967071110
hendridg <hendridg@gmail.com>
2022-01-04 18:25:42 -0400

    Fix linter error in index.js file

--- index.js
+++ index.js
@@ -1,81 +1,74 @@
 const listBooks = document.querySelector('.list-books');
+const form = document.querySelector('.form-input');
+const [title, author] = form.elements;
 
-  const form = document.querySelector('.form-input');
-  const [title, author] = form.elements;
+const inputBook = {};
+let books = new Array([]);
 
-  const inputBook = {};
-  let books = [];
-
-  if (localStorage.savedBooks) {
-    books = JSON.parse(localStorage.getItem('savedBooks'));
-  }
-
-  title.addEventListener('change', () => {
-    inputBook.title = title.value;
-  });
-
-  author.addEventListener('change', () => {
-    inputBook.author = author.value;
-  });
-
-class Book {
-  function Book(title, author) {
-    this.title = title;
-    this.author = author;
-  }
+if (localStorage.savedBooks) {
+  books = JSON.parse(localStorage.getItem('savedBooks'));
 }
+
+title.addEventListener('change', () => {
+  inputBook.title = title.value;
+});
+
+author.addEventListener('change', () => {
+  inputBook.author = author.value;
+});
 
 const populateFields = () => {
   localStorage.setItem('savedBooks', JSON.stringify(books));
 };
 
-class UI {
-  function removeBook(book) {
+const Book = class {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  static removeBook(book) {
     const result = books.filter((b) => b !== book);
     books = result;
     populateFields();
   }
-}
 
-  const displayBooks = () => {
+  static addBook = (newBook) => {
+    books.push(newBook);
+    populateFields();
+    this.displayBooks();
+  };
+
+  static displayBooks = () => {
     listBooks.innerHTML = '';
     books.map((book) => {
-      const bookDiv = document.createElement('div');
-      const titleBook = document.createElement('p');
-      const authorBook = document.createElement('p');
+      const bookDiv = document.createElement('tr');
+      const elementBook = document.createElement('td');
       const deleteBtn = document.createElement('button');
-      const hrElementet = document.createElement('hr');
       deleteBtn.textContent = 'Remove';
 
-      titleBook.textContent = book.title;
-      authorBook.textContent = book.author;
+      elementBook.textContent = `"${book.title}" by ${book.author}`;
 
-      bookDiv.appendChild(titleBook);
-      bookDiv.appendChild(authorBook);
+      bookDiv.classList.add('book-container');
+      bookDiv.appendChild(elementBook);
       bookDiv.appendChild(deleteBtn);
-      bookDiv.appendChild(hrElementet);
 
       listBooks.appendChild(bookDiv);
 
       deleteBtn.addEventListener('click', () => {
-        removeBook(book);
+        this.removeBook(book);
         listBooks.removeChild(bookDiv);
       });
       return listBooks;
     });
   };
+};
 
-  const addBook = (newBook) => {
-    books.push(newBook);
-    populateFields();
-    displayBooks();
-  };
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  Book.addBook(new Book(inputBook.title, inputBook.author));
+  form.submit();
+});
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    addBook(new Book(inputBook.title, inputBook.author));
-    form.submit();
-  });
-
-  displayBooks();
-  populateFields();
+Book.displayBooks();
+populateFields();
